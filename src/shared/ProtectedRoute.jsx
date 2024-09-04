e888daf1359a210255ca74d4db9aa2372240184d
박시은 <charm516@naver.com>
2024-09-04 15:23:20 +0900

    [feat] - ProtectedRouter 설정

--- src/shared/ProtectedRoute.jsx
+++ src/shared/ProtectedRoute.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { useAuth } from 'context/AuthContext';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-  return <div>ProtectedRoute</div>;
+  const { isSignIn } = useAuth();
+  const location = useLocation(); // 현재 페이지의 경로 정보를 가져옴
+
+  if (!isSignIn) {
+    return <Navigate to="/sign-in" state={{ from: location }} replace />;
+  }
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
