bbe41b1467c3ed47a1bf6e175029e7e97939e2b2
박시은 <charm516@naver.com>
2024-09-04 15:52:22 +0900

    [feat] - App.jsx AuthProvider 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { AuthProvider } from 'context/AuthContext';
 import Router from 'shared/Router';
 
 const App = () => {
-  return <Router />;
+  return (
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
+  );
 };
 
 export default App;
